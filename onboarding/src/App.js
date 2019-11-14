adfa005cfa875b12dac4cf7ac0d913830ba5e93b
rodrigograca31 <rodrigograca1993@gmail.com>
2019-11-14 19:02:26 +0000

    Stretch 💪

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,33 +1,45 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
 import UserFormWithFormik from "./components/UserForm";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
 	const [users, setUsers] = useState([
 		{
 			id: 1,
 			email: "rodrigo@mail.com",
-			name: "rodrigo"
+			name: "rodrigo",
+			role: "admin"
 		}
 	]);
 
 	return (
 		<div className="App">
-			<UserFormWithFormik
-				users={users}
-				setUsers={setUsers}
-			></UserFormWithFormik>
-			<hr />
-			{users.map(user => {
-				return (
-					<div>
-						<p>
-							{user.name} - {user.email} - {user.id}
-						</p>
-					</div>
-				);
-			})}
+			<Container>
+				<Row>
+					<Col md={{ size: 8, offset: 2 }}>
+						<UserFormWithFormik
+							users={users}
+							setUsers={setUsers}
+						></UserFormWithFormik>
+						<hr />
+						<h1>USERS:</h1>
+						{/* <hr /> */}
+						<div className="users">
+							{users.map(user => {
+								return (
+									<p>
+										{user.name} - {user.email} - {user.id} -{" "}
+										{user.role}
+									</p>
+								);
+							})}
+						</div>
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 }
--- onboarding/src/components/UserForm/index.jsx
+++ onboarding/src/components/UserForm/index.jsx
@@ -3,37 +3,160 @@ import { withFormik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
+import { Button, Label, Input, FormGroup, Alert } from "reactstrap";
+
 const UserForm = props => {
 	console.log(props);
 	const { values, errors, touched } = props;
 
 	return (
 		<Form>
-			<label>
-				Name:
-				<Field type="text" name="name" placeholder="Name please?" />
-			</label>
-			{touched.name && errors.name}
-			<br />
-			<label>
-				Email:
-				<Field type="email" name="email" placeholder="Email please?" />
-			</label>
-			{touched.email && errors.email}
-			<br />
-			<label>
-				Password:
-				<Field type="password" name="password" />
-			</label>
-			{touched.password && errors.password}
-			<br />
-			<label>
-				Terms:
-				<Field type="checkbox" name="terms" />
-			</label>
-			{touched.terms && errors.terms}
-			<br />
-			<input type="submit" value="Send" />
+			<FormGroup>
+				<Label>Name:</Label>
+				<Field
+					name="name"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input
+								{...field}
+								type="text"
+								placeholder="Name please?"
+							/>
+							{touched[field.name] && errors[field.name] && (
+								<Alert color="danger">
+									{errors[field.name]}
+								</Alert>
+							)}
+						</>
+					)}
+				/>
+				{/* {touched.name && errors.name} */}
+			</FormGroup>
+			<FormGroup>
+				<Label>Last name:</Label>
+				<Field
+					name="lname"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input
+								{...field}
+								type="text"
+								placeholder="Last Name please?"
+							/>
+							{touched[field.name] && errors[field.name] && (
+								<Alert color="danger">
+									{errors[field.name]}
+								</Alert>
+							)}
+						</>
+					)}
+				/>
+				{/* {touched.name && errors.name} */}
+			</FormGroup>
+			<FormGroup>
+				<Label>Age:</Label>
+				<Field
+					name="age"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input
+								{...field}
+								type="number"
+								placeholder="Age please?"
+							/>
+							{touched[field.name] && errors[field.name] && (
+								<Alert color="danger">
+									{errors[field.name]}
+								</Alert>
+							)}
+						</>
+					)}
+				/>
+				{/* {touched.name && errors.name} */}
+			</FormGroup>
+			<FormGroup>
+				<Label>Email:</Label>
+				<Field
+					name="email"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input
+								{...field}
+								type="email"
+								placeholder="Email please?"
+							/>
+							{touched[field.name] && errors[field.name] && (
+								<Alert color="danger">
+									{errors[field.name]}
+								</Alert>
+							)}
+						</>
+					)}
+				/>
+				{/* {touched.email && errors.email} */}
+			</FormGroup>
+			<FormGroup>
+				<Label>Password:</Label>
+				<Field
+					name="password"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input {...field} type="password" />
+							{touched[field.name] && errors[field.name] && (
+								<Alert color="danger">
+									{errors[field.name]}
+								</Alert>
+							)}
+						</>
+					)}
+				/>
+				{/* {touched.password && errors.password} */}
+			</FormGroup>
+			<FormGroup>
+				<Label>Terms:</Label>
+				<Field
+					name="terms"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<input
+								{...field}
+								type="checkbox"
+								checked={field.value}
+							/>
+							{touched[field.name] && errors[field.name] && (
+								<Alert color="danger">
+									{errors[field.name]}
+								</Alert>
+							)}
+						</>
+					)}
+				/>
+				{/* {touched.terms && errors.terms} */}
+			</FormGroup>
+			<FormGroup>
+				<Label>Role:</Label>
+				<Field
+					name="role"
+					render={({ field, form: { touched, errors } }) => (
+						<>
+							<Input type="select" {...field}>
+								<option value="choose">Choose One</option>
+								<option value="admin">Admin</option>
+								<option value="user">User</option>
+							</Input>
+							{touched[field.name] && errors[field.name] && (
+								<Alert color="danger">
+									{errors[field.name]}
+								</Alert>
+							)}
+						</>
+					)}
+				/>
+				{/* {touched.terms && errors.terms} */}
+			</FormGroup>
+			<Button type="submit" value="Send" color="success">
+				Save User!
+			</Button>
 		</Form>
 	);
 };
@@ -42,21 +165,33 @@ const UserFormWithFormik = withFormik({
 	mapPropsToValues() {
 		return {
 			name: "",
+			lname: "",
+			age: "",
 			email: "",
 			password: "",
-			terms: false
+			terms: false,
+			role: ""
 		};
 	},
 
 	validationSchema: Yup.object().shape({
 		name: Yup.string().required("Please enter your first name"),
+		lname: Yup.string().required("Please enter your last name"),
+		age: Yup.number()
+			.min(18, "You must be older than 18")
+			.max(99, "You must be younger than 99")
+			.required("Please enter your age"),
 		email: Yup.string()
 			.email()
-			.required("Please enter your email"),
+			.required("Please enter your email")
+			.notOneOf(["waffle@syrup.com"], "That email is already taken"),
 		password: Yup.string()
 			.min(8, "At least 8 chars")
 			.required("Please enter your password"),
-		terms: Yup.boolean().oneOf([true], "Must Accept Terms")
+		terms: Yup.boolean().oneOf([true], "Must Accept Terms"),
+		role: Yup.string()
+			.oneOf(["admin", "user"], "Please choose a role")
+			.required("Please choose a role")
 	}),
 
 	handleSubmit(values, tools) {
